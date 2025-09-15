bbbefd27149a86d8fe0355ba481e841524f37bc6
ggoswami777 <gaurav.goswami1304@gmail.com>
2025-09-15 20:25:50 +0530

    added background in hero section

---
+++ civic-issue-reporting-system/src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import worldBackground from '../../assets/world.png'
+const Hero = () => {
+  return (
+    <div className='h-[90vh] relative flex justify-center'>
+      <img className='w-full h-full  absolute -left-150 object-center ' src={worldBackground}/>
+      <div>
+    
+      </div>
+    </div>
+    
+  )
+}
+
+export default Hero
