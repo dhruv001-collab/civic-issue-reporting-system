27227ab9332be15ac4fcdac0076eb360068bc05f
dhruv001-collab <hachathon001@gmail.com>
2025-09-16 10:31:24 +0530

    Save local changes

--- civic-issue-reporting-system/src/Context/AppContext.jsx
+++ civic-issue-reporting-system/src/Context/AppContext.jsx
@@ -3,6 +3,8 @@ import { createContext } from "react";
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+
+
     const value = {
         // Add any global state or functions you want to provide here
     };
