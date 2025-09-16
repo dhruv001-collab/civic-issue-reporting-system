9c880720c035fa73b160511af9efa1ceb7363d7a
dhruv001-collab <hachathon001@gmail.com>
2025-09-16 15:57:59 +0530

    implemented login feature

--- civic-issue-reporting-system/src/main.jsx
+++ civic-issue-reporting-system/src/main.jsx
@@ -1,14 +1,24 @@
+import './index.css'
+import App from './App.jsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter} from "react-router-dom";
 import { AppContextProvider } from './Context/AppContext.jsx'
-import './index.css'
-import App from './App.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+  // Import your Publishable Key
+  const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+  if (!PUBLISHABLE_KEY) {
+    throw new Error('Add your Clerk Publishable Key to the .env file')
+  }
 
 createRoot(document.getElementById('root')).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl={"/"}>
   <BrowserRouter>
     <AppContextProvider>
       <App />
     </AppContextProvider>
   </BrowserRouter>
+  </ClerkProvider>
 )
