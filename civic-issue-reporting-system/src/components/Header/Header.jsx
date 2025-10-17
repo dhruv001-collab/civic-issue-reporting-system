b52321c4d7f462d3ba9dc4c7db6f60427bb77124
ggoswami777 <gaurav.goswami1304@gmail.com>
2025-10-17 16:42:23 +0530

    fixed some routes

--- civic-issue-reporting-system/src/components/Header/Header.jsx
+++ civic-issue-reporting-system/src/components/Header/Header.jsx
@@ -5,14 +5,12 @@ import logo from "../../assets/logo.png";
 import { AppContext } from "../../Context/AppContext";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
-
 const Nav = () => {
   const { openSignIn } = useClerk();
   const { user } = useUser();
   const navigate = useNavigate();
 
   const { isOpen, setIsOpen } = useContext(AppContext);
-
 
   const [isScrolled, setIsScrolled] = useState(false);
 
@@ -26,14 +24,20 @@ const Nav = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-
   return (
     <>
-      <nav className={`bg-white shadow-md px-3 lg:px-12 sticky top-0 left-0 w-full z-30 ${isScrolled ? "bg-white/60 backdrop-blur-sm shadow-md" : "bg-white"}`}>
+      <nav
+        className={`bg-white shadow-md px-3 lg:px-12 sticky top-0 left-0 w-full z-30 ${
+          isScrolled ? "bg-white/60 backdrop-blur-sm shadow-md" : "bg-white"
+        }`}
+      >
         <div className="flex justify-between items-center">
           {/* Left Section*/}
           <div className="flex items-center">
-            <div className="h-[100px] w-[120px] sm:h-[70px] sm:w-[160px] lg:h-[100px] lg:w-[180px] " onClick={() => navigate("/")} >
+            <div
+              className="h-[100px] w-[120px] sm:h-[70px] sm:w-[160px] lg:h-[100px] lg:w-[180px] "
+              onClick={() => navigate("/")}
+            >
               <img
                 src={logo}
                 alt="logo"
@@ -47,38 +51,39 @@ const Nav = () => {
             <NavLink
               to="/"
               className={({ isActive }) =>
-                `transition-all ${isActive
-                  ? "text-blue-500 font-semibold"
-                  : "hover:text-blue-500"
+                `transition-all ${
+                  isActive
+                    ? "text-blue-500 font-semibold"
+                    : "hover:text-blue-500"
                 }`
               }
             >
               Home
             </NavLink>
 
-            {user ?
-
+            {user ? (
               <NavLink
                 to="/dashboard"
                 className={({ isActive }) =>
-                  `transition-all ${isActive
-                    ? "text-blue-500 font-semibold"
-                    : "hover:text-blue-500"
+                  `transition-all ${
+                    isActive
+                      ? "text-blue-500 font-semibold"
+                      : "hover:text-blue-500"
                   }`
                 }
               >
                 Dashboard
               </NavLink>
-              :
+            ) : (
               ""
-
-            }
+            )}
             <NavLink
               to="/stats"
               className={({ isActive }) =>
-                `transition-all ${isActive
-                  ? "text-blue-500 font-semibold"
-                  : "hover:text-blue-500"
+                `transition-all ${
+                  isActive
+                    ? "text-blue-500 font-semibold"
+                    : "hover:text-blue-500"
                 }`
               }
             >
@@ -88,9 +93,10 @@ const Nav = () => {
             <NavLink
               to="/faqs"
               className={({ isActive }) =>
-                `transition-all ${isActive
-                  ? "text-blue-500 font-semibold"
-                  : "hover:text-blue-500"
+                `transition-all ${
+                  isActive
+                    ? "text-blue-500 font-semibold"
+                    : "hover:text-blue-500"
                 }`
               }
             >
@@ -100,9 +106,10 @@ const Nav = () => {
             <NavLink
               to="/reports"
               className={({ isActive }) =>
-                `transition-all ${isActive
-                  ? "text-blue-500 font-semibold"
-                  : "hover:text-blue-500"
+                `transition-all ${
+                  isActive
+                    ? "text-blue-500 font-semibold"
+                    : "hover:text-blue-500"
                 }`
               }
             >
@@ -111,9 +118,10 @@ const Nav = () => {
             <NavLink
               to="/contact"
               className={({ isActive }) =>
-                `transition-all ${isActive
-                  ? "text-blue-500 font-semibold"
-                  : "hover:text-blue-500"
+                `transition-all ${
+                  isActive
+                    ? "text-blue-500 font-semibold"
+                    : "hover:text-blue-500"
                 }`
               }
             >
@@ -129,34 +137,40 @@ const Nav = () => {
               </button>
             </Link>
             <p className="flex justify-center items-center text-2xl">|</p>
-            {user ?
-              <div className='flex items-center gap-4 max-sm:text-sm'>
-
-                <p className=''>
-                  <span className="font-bold">
-                    Hi
-                  </span>
-
-                  , {user.firstName ? user.firstName.charAt(0).toUpperCase() + user.firstName?.slice(1).toLowerCase() : ""}
-                  {" "}
-                  {user.lastName ? user.lastName.charAt(0).toUpperCase() + user.lastName?.slice(1).toLowerCase() : ""}</p>
+            {user ? (
+              <div className="flex items-center gap-4 max-sm:text-sm">
+                <p className="">
+                  <span className="font-bold">Hi</span>,{" "}
+                  {user.firstName
+                    ? user.firstName.charAt(0).toUpperCase() +
+                      user.firstName?.slice(1).toLowerCase()
+                    : ""}{" "}
+                  {user.lastName
+                    ? user.lastName.charAt(0).toUpperCase() +
+                      user.lastName?.slice(1).toLowerCase()
+                    : ""}
+                </p>
                 <UserButton afterSignOutUrl="/" />
               </div>
-              :
-              <button onClick={() => openSignIn()} className="px-6 py-2 border border-black rounded-full hover:bg-gray-100 transition-all cursor-pointer">
+            ) : (
+              <button
+                onClick={() => openSignIn()}
+                className="px-6 py-2 border border-black rounded-full hover:bg-gray-100 transition-all cursor-pointer"
+              >
                 Sign In
               </button>
-            }
-
+            )}
           </div>
 
           {/* Hamburger Icon */}
 
           <div className="lg:hidden flex items-center gap-4">
             <div className={!isOpen ? "flex" : "hidden"}>
-              <button className="px-6 py-2 text-sm bg-teal-500 text-white rounded-full hover:bg-teal-600 transition-all">
-                Post Issue
-              </button>
+              <Link to="/report-issue">
+                <button className="px-6 py-2  bg-teal-500 text-white rounded-full hover:bg-teal-600 transition-all cursor-pointer">
+                  Post Issue
+                </button>
+              </Link>
             </div>
             <UserButton
               className={!isOpen ? "hidden" : "flex"}
@@ -170,8 +184,9 @@ const Nav = () => {
 
         {/* Mobile Menu */}
         <div
-          className={`lg:hidden overflow-hidden transition-all duration-500 ease-in-out  ${isOpen ? "max-h-[500px] opacity-100 mt-4" : "max-h-0 opacity-0"
-            }`}
+          className={`lg:hidden overflow-hidden transition-all duration-500 ease-in-out  ${
+            isOpen ? "max-h-[500px] opacity-100 mt-4" : "max-h-0 opacity-0"
+          }`}
         >
           <div className="flex flex-col gap-4 font-medium text-gray-700 text-lg">
             <Link to="/" onClick={() => setIsOpen(false)}>
@@ -194,9 +209,11 @@ const Nav = () => {
             </Link>
 
             <div className="flex flex-col gap-3 mt-3">
-              <button className="px-6 py-2  bg-teal-500 text-white rounded-full hover:bg-teal-600 transition-all">
-                Post Issue
-              </button>
+              <Link to="/report-issue">
+                <button className="px-6 py-2  bg-teal-500 text-white rounded-full hover:bg-teal-600 transition-all cursor-pointer">
+                  Post Issue
+                </button>
+              </Link>
               {user ? (
                 <div className="flex items-center justify-center gap-4 max-sm:text-sm">
                   <p className="font-bold">
